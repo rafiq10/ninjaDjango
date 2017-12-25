dec4139358dc5300a582e526c8a571bea75889f7
rafiq10 <rafiq10@gmail.com>
2017-12-25 10:57:34 +0100

    First ninja Django project commit

---
+++ djangonautic/djangonautic/urls.py
@@ -0,0 +1,13 @@
+"""
+Django settings for djangonautic project.
+"""
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/', views.about),
+    path('', views.home),
+]
