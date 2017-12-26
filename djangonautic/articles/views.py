157fa9a6c060861788966ff61da2b491767a5d69
rafiq10 <rafiq10@gmail.com>
2017-12-26 11:58:40 +0100

    End of chapter 24-requiring-login

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
-
+from django.contrib.auth.decorators import login_required
 
 def articles_list(request):
     articles = Article.objects.all().order_by('date')
@@ -12,3 +12,7 @@ def article_detail(request,slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug = slug)
     return render(request,'articles/article_detail.html',{'article':article})
+
+@login_required(login_url='/accounts/login/')
+def article_create(request):
+    return render(request,'articles/article_create.html')
